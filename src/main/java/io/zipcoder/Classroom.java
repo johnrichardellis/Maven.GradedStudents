a5bd943bb88a95543fdacc96d080b538129b46cc
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-12-05 16:33:11 -0500

    updates and notes

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -2,31 +2,61 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Classroom {
 
-    private Student[] student;
+    private Student[] student;  // list of students, can be used to initialize
     private Integer maxNUmberOfStudents;
 
-    public Classroom(int maxNUmberOfStudents) {
-        this.student = new Student[maxNUmberOfStudents];
+    // redoing constructors for practice
+    public Classroom (int maxNumberOfStudents) {
+        this.student = new Student[maxNumberOfStudents];
     }
 
-    public Classroom(Student[] students) {
+    public Classroom (Student[] students) {
         this.student = students;
     }
 
-    // nullary constructor sets empty array size 30
-    public void Classroom() {
+    public Classroom () {
         this.student = new Student[30];
     }
 
+//    public Classroom(int maxNUmberOfStudents) {
+//
+//        this.student = new Student[maxNUmberOfStudents];
+//    }
+//
+//    public Classroom(Student[] students) {
+//
+//        this.student = students;
+//    }
+//
+//
+//    public void Classroom() {
+//
+//        // nullary constructor sets empty array size 30
+//        this.student = new Student[30];
+//    }
+
     public Student[] getStudents() {
+
         return student;
     }
 
     public Double getAverageExamScore() {
+
+//        // again for practice
+//        Double sum = 0.0;
+//
+//        for (Student thisStudent : student) {
+//            sum = sum + thisStudent.getAverageExamScore();
+//        }
+//        return sum / student.length;
+
+
+
         // declare sum variable to keep track
         Double sum = 0.0;
 
@@ -40,6 +70,23 @@ public class Classroom {
     }
 
     public void addStudent(Student newStudent) {
+
+
+        // cannot simply do student.add(newStudent) here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // looping through length of students array
         for (int i = 0; i < student.length - 1; i++) {
             // if the student at i equals null
@@ -58,15 +105,30 @@ public class Classroom {
 
         // loop through studentList we created
         for (int i = 0; i < student.length; i++) {
+            // check to see if the firstName and lastName match what we are given
             if (student[i].getFirstName().equals(firstName) && student[i].getLastName().equals(lastName)) {
-
+                    // if student matches, remove that particular student
+                    studentList.remove(student[i]);
+                    // add null
+                    // arrays cannot change size, so you cannot remove without placing something
+                    // in its place. in this case we would replace with null
+                    studentList.add(null);
             }
         }
     }
 
     public void getStudentByScore() {
-        // create new arraylist
-        Arrays.sort(student);
+//        List<Student> studentbyScore = new ArrayList<>(Arrays.asList(student));
+//
+//        Comparator<Student> byExamScores = Comparator.comparing(Student::getAverageExamScore);
+//        Comparator<Student> byFullName = Comparator.comparing(Student::getFullName);
+//
+//        studentbyScore.sort(byExamScores.reversed().thenComparing(byFullName));
+//
+//        return studentbyScore.toArray(new Student[0]);
+
+
+
     }
 
     public void getGradeBook() {
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -17,30 +17,37 @@ public class Student {
 
         this.firstName = firstName;
         this.lastName = lastName;
+        // below is representative of a collection of testScores
+        // new arraylist of type Double passing in testScores variable
         this.testScores = new ArrayList<Double>(Arrays.asList(testScores));
     }
 
 
     // part 1.3
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getExamScores() {
 
         String result = "";
+        // loop through and add to result string each time
         for (int i = 0; i < testScores.size(); i++) {
              result = result + "Exam " + (i + 1) + " score: " + testScores.get(i) + "\n";
         }
@@ -49,6 +56,7 @@ public class Student {
     }
 
     public Integer getNumberOfExamsTaken() {
+
         return testScores.size();
     }
 
@@ -63,16 +71,28 @@ public class Student {
 
 
     public void addExamScore(Double examScore) {
+
+        // adding parameter examScore to testScores list instance field
+        // listName.add(itemToAddToList)
         testScores.add(examScore);
     }
 
 
     public void setExamScore(Integer examNumber, Double newScore) {
 
+        // setting a new score to a specific exam in the list
+        // listName.set(examIndex (examNumber - 1 to access that index), then the actual score)
         testScores.set(examNumber - 1, newScore);
     }
 
     public Double getAverageExamScore() {
+
+        // we want to get total amount between all the scores, right?
+        // then divide by how many tests we have
+        // loop through and add each score to the previous total that we had
+        // use the .get method to get the current score to add
+        // finally, divide by .size method
+
         Double result = 0.0;
         for (int i = 0; i < testScores.size(); i++) {
             result = result + testScores.get(i);
@@ -82,6 +102,11 @@ public class Student {
 
     @Override
     public String toString() {
+
+
+        // simply doing an override, and then figuring out the exact formatting
+        // to return it how they want
+
         String string = "";
         String result = string += "Student name: " + firstName + " " + lastName + "\n" +
                 "> Avg Score: " + this.getAverageExamScore() + "\n" + "> Exam scores: " +
